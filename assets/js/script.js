71bae55e99b6627fe38662b5fc399ee91628a7cb
Philip Jambrisak <jambrisak@gmail.com>
2022-11-13 20:25:33 +0100

    fixed variables naming

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //variables
 const btn_primary = document.querySelector(".btn_primary button");
-const modal_box = document.querySelector(".btn_primary button");
-const modal_quit = modal_box.querySelector(".buttons modal_quit");
-const modal_start = document.querySelector(".buttons modal_start");
+const modal_box = document.querySelector(".modal_box");
+const modal_quit = modal_box.querySelector(".buttons .modal_quit");
+const modal_start = document.querySelector(".buttons .modal_start");
