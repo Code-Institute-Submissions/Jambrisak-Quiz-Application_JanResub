c011ce239c196d40c76d19fded028c7117c6d3cc
Philip Jambrisak <jambrisak@gmail.com>
2022-11-13 20:27:08 +0100

    add onclick event

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ const btn_primary = document.querySelector(".btn_primary button");
 const modal_box = document.querySelector(".modal_box");
 const modal_quit = modal_box.querySelector(".buttons .modal_quit");
 const modal_start = document.querySelector(".buttons .modal_start");
+
+//If statement for start quiz button, when clicked
+btn_primary.onclick = () =>{
+    modal_box.classList.add("");
+}
