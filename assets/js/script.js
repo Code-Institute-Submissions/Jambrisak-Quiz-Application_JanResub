84c50b7c1170e3065852c515d584190a94822eed
Philip Jambrisak <jambrisak@gmail.com>
2022-11-13 20:22:48 +0100

    add variables for basic elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,5 @@
 //variables
 const btn_primary = document.querySelector(".btn_primary button");
+const modal_box = document.querySelector(".btn_primary button");
+const modal_quit = modal_box.querySelector(".buttons modal_quit");
+const modal_start = document.querySelector(".buttons modal_start");
